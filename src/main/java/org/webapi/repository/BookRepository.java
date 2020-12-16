4c0085a924d9b14f6ca73c9ac35e3fea929cf7af
ben-decosta <benpanjaitan02@gmail.com>
2020-12-16 15:27:24 +0700

    first commit

---
+++ src/main/java/org/webapi/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package org.webapi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.webapi.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+	List<Book> findByPublished(boolean published);
+	List<Book> findByTitleContaining(String title);
+}
